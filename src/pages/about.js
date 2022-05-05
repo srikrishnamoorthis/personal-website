e10e20d9909c3b3e112e2e617c54c6a82a3aaba9
Sri Krishnamoorthi S <srikriss@netapp.com>
2022-05-05 22:34:37 +0530

    About page checkpoint

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+const introduction = 'I am Sri Krishnamoorthi, UI developer with around 6 years of industry experience. Currently, working at NetApp. I \
+    primarily use React, HTML, CSS, Javascript, node to create perfomant UI';
 
 About.propTypes = {};
 About.displayName = 'About';
 
 function About(props) {
     return (
-        <div>About</div>
+        <div className='dark:text-white text-black p-8'>
+            {introduction}
+        </div>
     )
 }
 
